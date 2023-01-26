791cb35ce75c3fdcf14b276a4da39d98814f1c9a
Chet Murthy <chetsky@gmail.com>
2023-01-25 19:06:37 -0800

    meh

--- tests/ppx_perl.ml
+++ tests/ppx_perl.ml
@@ -4,8 +4,11 @@ open OUnit2
 let test_simple ctxt =
   let printer x = x in
   assert_equal ~printer "abc"  (Re.Group.get ([%match "abc"/exc] "abc") 0)
+  ; assert_equal None  ([%match "abc"] "abd")
+  ; assert_equal None  ([%match "abc"/strings] "abd")
   ; assert_equal "abc"  ([%match "abc"/exc strings] "abc")
   ; assert_equal ("abc", Some "b")  ([%match "a(b)c"/exc strings] "abc")
+  ; assert_equal ("ac", None)  ([%match "a(?:(b)?)c"/exc strings] "ac")
 
 let suite = "Test pa_ppx_string" >::: [
       "simple"   >:: test_simple
