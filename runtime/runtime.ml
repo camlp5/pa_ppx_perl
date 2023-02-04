5a8b9e2a0a414db43e35a9d772dd822ebe6a26ca
Chet Murthy <chetsky@gmail.com>
2023-02-03 17:31:20 -0800

    cleanup

--- runtime/runtime.ml
+++ runtime/runtime.ml
@@ -1,12 +1,5 @@
 (* camlp5o *)
 (* runtime.ml,v *)
-(* Copyright (c) 2020--2022 Jane Street Group, LLC <opensource@janestreet.com> *)
-(* used under the Jane Street MIT License for ppx_string *)
-
-let pad str len =
-  let pad_len = max 0 (len - String.length str) in
-  let padding = String.make pad_len ' ' in
-  padding ^ str
 
 let pcre_full_split rex str =
   let open Pcre in
