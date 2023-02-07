753b0b74123500d111e7875e7b2f2d65fcbf5e52
Chet Murthy <chetsky@gmail.com>
2023-02-07 11:17:08 -0800

    missing files checkin

---
+++ tests/standalone_test.ml
@@ -0,0 +1,9 @@
+(** -syntax camlp5o -ppopt -pa_ppx_regexp-nostatic *)
+open OUnit2
+
+let _ = [%match "abc"]
+let _ = [%match "abc"/pcre]
+let _ = [%split "a"]
+let _ = [%split "a"/pcre]
+let _ = [%subst ".+" / {|<<$0>>|}]
+let _ = [%subst ".+" / {|<<$0>>|}/pcre]
